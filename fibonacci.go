1d6ca46bc2be167d302a0d99e0d6e4a1e68e1f35
aosindo <andyovvo8@gmail.com>
2024-03-04 18:28:22 +0300

    fibonacci

--- fibonacci.go
+++ fibonacci.go
@@ -1,10 +1,10 @@
 package piscine
 
 func Fibonacci(index int) int {
-	if index <= 1 {
-		return index
-	} else if index < 0 {
+	if index < 0 {
 		return -1
+	} else if index <= 1{
+		return index
 	} else {
 		return Fibonacci(index-1) + Fibonacci(index-2)
 	}
