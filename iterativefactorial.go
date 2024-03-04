83dbcbf3e1747060d5e99f55def61085e5c28483
aosindo <andyovvo8@gmail.com>
2024-03-04 12:03:01 +0300

    interative

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,8 +1,10 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb <= 0 || nb >= 5 {
+	if nb < 0 || nb >= 5 {
 		return 0
+	} else if nb == 0 {
+		return 1
 	} else {
 		result := 1
 		for i := nb; i >= 1; i-- {
