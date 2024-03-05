211919291f45da615ce28874926c89582c78e848
aosindo <andyovvo8@gmail.com>
2024-03-05 15:30:57 +0300

    sqrt

---
+++ sqrt.go
@@ -0,0 +1,18 @@
+package piscine
+
+func Sqrt(nb int) int {
+	if nb < 0 {
+		return 0
+	}
+
+	first := nb/2
+
+	for first := 1; first < nb; first++ {
+	}
+	if first*first == nb {
+		return first
+	} else {
+	return 0
+    }
+}
+
