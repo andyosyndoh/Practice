78615b24c7f3d182022d1ed3464d4abd81c41d7a
aosindo <andyovvo8@gmail.com>
2024-03-05 15:32:45 +0300

    sqrt

--- sqrt.go
+++ sqrt.go
@@ -5,14 +5,14 @@ func Sqrt(nb int) int {
 		return 0
 	}
 
-	first := nb/2
+	first := nb / 2
 
 	for first := 1; first < nb; first++ {
 	}
 	if first*first == nb {
 		return first
 	} else {
-	return 0
+		return 0
     }
 }
 
