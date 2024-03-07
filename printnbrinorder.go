186cda9f1ef7be8fadb99e21615fa03250e73e99
aosindo <andyovvo8@gmail.com>
2024-03-07 22:57:27 +0300

    printnbr

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -4,15 +4,20 @@ import "github.com/01-edu/z01"
 
 func PrintNbrInOrder(n int) {
 	if n < 0 {
-		z01.PrintRune(0)
+		return
 	}
 	digits := make([]int, 10)
 	result := make([]rune, 0, 10)
 	for n > 0 {
-		digits[n%10]++
+		digit := n%10
+		digits[digit]++
 		n /= 10
 	}
-	for i := 0; i < 10; i++ {
+	
+	for i :=0; i < 10; i++ {
+		if i == 0 {
+			result = append(result, '0')
+		}
 		for j := 0; j < digits[i]; j++ {
 			result = append(result, rune(i)+'0')
 		}
@@ -20,4 +25,5 @@ func PrintNbrInOrder(n int) {
 	for _, digit := range result {
 		z01.PrintRune(digit)
 	}
+	z01.PrintRune(0)
 }
