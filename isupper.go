b76bd4f3b6be366d92230605582bd5d971207bcc
aosindo <andyovvo8@gmail.com>
2024-03-07 13:52:33 +0300

    isupper

--- isupper.go
+++ isupper.go
@@ -1,10 +1,10 @@
 package piscine
 
 func IsUpper(s string) bool {
-	for _, char := range s {
-		if char >= 'A' && char <= 'Z' {
-			return true
+	for i := 0; i < len(s); i++ {
+		if s[i] < 'A' || s[i] > 'Z' {
+			return false
 		}
 	}
-	return false
+	return true
 }
