f8dcc485510f8731ba768cc5206c6fd15122f8be
aosindo <andyovvo8@gmail.com>
2024-03-07 12:47:50 +0300

    index

--- index.go
+++ index.go
@@ -1,16 +1,12 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	a := []rune(s)
-	b := []rune(toFind)
-
-	if len(s) == 0 || len(toFind) == 0 {
-		return 0
-	}
-	for i := 0; i <= len(s); i++ {
-		if a[i] == b[0] {
+	h := len(s)
+	k := len(toFind)
+	for i := 0; i <= h-k; i++ {
+		if s[i:i+k] == toFind {
 			return i
 		}
 	}
-	return 0
+	return -1
 }
