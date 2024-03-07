485d5b052eed61d6cccac0a813fca022b93136d2
aosindo <andyovvo8@gmail.com>
2024-03-07 14:25:36 +0300

    isalpha

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for i := 0; i < len(s); i++ {
-		if (s[i] < 'a' || s[i] > 'z') && (s[i] < 'A' || s[i] > 'Z') && (s[i] < '1' || s[i] > '9') {
+		if (s[i] < 'a' || s[i] > 'z') && (s[i] < 'A' || s[i] > 'Z') && (s[i] < '1' || s[i] > '0') {
 			return false
 		}
 	}
