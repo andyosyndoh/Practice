fcb14ef7f655026abeb7f68cdfb6040934b16921
aosindo <andyovvo8@gmail.com>
2024-03-07 17:58:04 +0300

    to upper

--- toupper.go
+++ toupper.go
@@ -4,8 +4,8 @@ func ToUpper(s string) string {
 	new := ""
 	for i := 0; i < len(s); i++ {
 		if s[i] >= 'a' && s[i] <= 'z' {
-			new = string(s[i]- 32) + string(s)
-		} 
+			new = string(s[i]-32) + string(s)
+		}
 	}
 	return new
 }
