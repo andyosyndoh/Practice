a0b85de4722ad255ee2512316e57fd1f870c7304
aosindo <andyovvo8@gmail>
2024-03-09 11:11:22 +0300

    appendrange

--- appendrange.go
+++ appendrange.go
@@ -2,7 +2,7 @@ package piscine
 
 func AppendRange(min, max int) []int {
 	var answer []int
-	for i := min - 1; i < max - 1; i++ {
+	for i := min - 1; i < max-1; i++ {
 		answer = append(answer, i+1)
 	}
 	return answer
