1179b62becebb6bed8964317e3ea9acb1a06d31f
aosindo <andyovvo8@gmail.com>
2024-03-11 15:08:31 +0300

    boolen

---
+++ boolen/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+
+	"os"
+)
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func even(nbr int) int {
+	if nbr%2 == 0 {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func isEven(nbr int) bool {
+	if even(nbr) == 1 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	args := os.Args[1:]
+	lengthOfArg := len(args) 
+	if isEven(lengthOfArg) {
+		printStr("I have an even number of arguments")
+	} else {
+		printStr("I have an odd number of arguments")
+	}
+}
