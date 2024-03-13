115ec2e6e39345e0b573fb83b8cad0092a7a4f68
aosindo <andyovvo8@gmail.com>
2024-03-13 15:36:45 +0300

    abort

--- abort.go
+++ abort.go
@@ -1,11 +1,11 @@
 package piscine
 
 func Abort(a, b, c, d, e int) int {
-	k := []int{a,b,c,d,e}
+	k := []int{a, b, c, d, e}
 	for a := 0; a < len(k); a++ {
-		for b := a+1; b < len(k); b++ {
-			if k[a]>k[b] {
-				k[a],k[b] = k[b],k[a]
+		for b := a + 1; b < len(k); b++ {
+			if k[a] > k[b] {
+				k[a], k[b] = k[b], k[a]
 			}
 		}
 	}
