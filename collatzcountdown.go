2d056f9a4239efd8cc9a0aef65b017736bf78ef2
aosindo <andyovvo8@gmail.com>
2024-03-13 14:19:39 +0300

    collatz

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -7,7 +7,7 @@ func CollatzCountdown(start int) int {
 	steps := 0
 	for start > 1 {
 		if start%2 == 0 {
-			start = start/2
+			start = start / 2
 		} else if start%2 != 0 {
 			start = (start*3 + 1)
 		}
