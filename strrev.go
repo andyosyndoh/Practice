57e8d0efa530af1d533e834d495c90e69d4342b2
aosindo <andyovvo8@gmail.com>
2024-03-18 09:51:57 +0300

    strrev

--- strrev.go
+++ strrev.go
@@ -3,7 +3,7 @@ package piscine
 func StrRev(s string) string {
 	x := []rune(s)
 	var new []rune
-	for i := len(s)-1; i >= 0; i-- {
+	for i := len(s) - 1; i >= 0; i-- {
 		new = append(new, x[i])
 	}
 	return string(new)
