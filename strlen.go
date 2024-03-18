99062bf5984625f14923906662fec32e83078240
Christopher Fremond <dev@01-edu.org>
2024-03-18 05:26:24 +0000

    Update 'strlen.go'

--- strlen.go
+++ strlen.go
@@ -2,8 +2,10 @@ package piscine
 
 func StrLen(s string) int {
   count := 0
-  for i := 1; i < len(s); i++ {
-    count++
+  for i := 0; i < len(s); i++ {
+    if i >= 'A' || i <= 'z' {
+      count++
+    }
   }
   return count
 }
