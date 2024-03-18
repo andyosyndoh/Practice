9e261db4bb935fd9c6708bb93042627fdacc4aa8
Christopher Fremond <dev@01-edu.org>
2024-03-18 05:40:08 +0000

    Update 'strlen.go'

--- strlen.go
+++ strlen.go
@@ -3,7 +3,7 @@ package piscine
 func StrLen(s string) int {
   count := 0
   for i := 0; i < len(s); i++ {
-    if s[i] >= 32 && s[i] <= 126 {
+    if (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 32 && s[i] <= 33 ){
       count++
     }
   }
