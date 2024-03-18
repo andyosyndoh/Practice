4e8cb0544c495a5201180297880e32dbb1a03bbb
Christopher Fremond <dev@01-edu.org>
2024-03-18 15:16:06 +0000

    Add 'listsize.go'

---
+++ listsize.go
@@ -0,0 +1,33 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushFront(l *List, data interface{}) {
+	n := &NodeL{Data: data}
+
+	if l.Head == nil {
+		l.Head = n
+		l.Tail = n
+	} else {
+		n.Next = l.Head
+		l.Head = n
+	}
+}
+
+func ListSize(l *List) int {
+  count := 0
+  new := l.Head
+  if new != nil {
+    count++
+    new = new.Next
+  }
+  return count
+}
