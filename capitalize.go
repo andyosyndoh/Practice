9251eab9420612c6f236a0e2a6e876f3710b8516
Christopher Fremond <dev@01-edu.org>
2024-03-20 21:18:44 +0000

    Update 'capitalize.go'

--- capitalize.go
+++ capitalize.go
@@ -4,16 +4,18 @@ func Capitalize(s string) string {
 	runes := []rune(s)
 
 	for i := range runes {
-		if runes[i] == ' ' ||  runes[i] == '+' {
-            if runes[i+1] >= 'a' && runes[i+1] <= 'z' {
-              runes[i+1] = runes[i+1] - 'a' + 'A'
-            } else {
-              runes[i+1] = runes[i+1]
-            }
-          
-        }
-    }
+		if runes[i] != runes[0] {
+			if runes[i] == ' ' || runes[i] == '+' {
+				if runes[i+1] >= 'a' && runes[i+1] <= 'z' {
+					runes[i+1] = runes[i+1] - 'a' + 'A'
+				} else if runes[i] >= 'A' && runes[i] <= 'Z' {
+					runes[i] = runes[i] - 'A' + 'a'
+
+				} else {
+					runes[i+1] = runes[i+1]
+				}
+			}
+		}
+	}
 	return string(runes)
 }
-
-
