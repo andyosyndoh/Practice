4ac27da4d738ba40584ef8df0550685dc6e4ed0a
aosindo <andyovvo8@gmail.com>
2024-03-20 17:02:30 +0300

    btreebinary

--- btreeisbinary.go
+++ btreeisbinary.go
@@ -2,12 +2,12 @@ package piscine
 
 func BTreeIsBinary(root *TreeNode) bool {
 	if root == nil {
+		return true
+	}
+	if root.Left != nil && root.Left.Data > root.Data {
 		return false
 	}
-	if root.Left != nil && root.Data < root.Left.Data {
-		return false
-	}
-	if root.Right != nil && root.Data > root.Right.Data {
+	if root.Right != nil && root.Right.Data < root.Data {
 		return false
 	}
 	if !BTreeIsBinary(root.Left) || !BTreeIsBinary(root.Right) {
