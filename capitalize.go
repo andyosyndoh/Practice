1f46636b5d753dd083562eb31a0cc71f1c4584e1
Christopher Fremond <dev@01-edu.org>
2024-03-21 06:06:57 +0000

    Update 'capitalize.go'

--- capitalize.go
+++ capitalize.go
@@ -1,19 +1,27 @@
 package piscine
 
+func check(a rune) bool {
+	if (a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z') || (a >= '0' && a <= '9') {
+		return true
+	}
+	return false
+}
+
 func Capitalize(s string) string {
 	runes := []rune(s)
 
+	first := true
+
 	for i := range runes {
-		if runes[i] != runes[0] {
-			if runes[i] == ' ' || runes[i] == '+' {
-				if runes[i+1] >= 'a' && runes[i+1] <= 'z' {
-					runes[i+1] = runes[i+1] - 'a' + 'A'
-				} else if runes[i] >= 'A' && runes[i] <= 'Z' {
-					runes[i] = runes[i] - 'A' + 'a'
-				} else {
-					runes[i+1] = runes[i+1]
-				}
+		if check(runes[i]) == true && first {
+			if runes[i] >= 'a' && runes[i] <= 'z' {
+				runes[i] -= 32
 			}
+			first = false
+		} else if runes[i] >= 'A' && runes[i] <= 'Z' {
+			runes[i] += 32
+		} else if check(runes[i]) == false {
+			first = true
 		}
 	}
 	return string(runes)
