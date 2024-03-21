3543d8b45c0899ab07b50a9bb6001b88838304b5
aosindo <andyovvo8@gmail.com>
2024-03-21 17:51:47 +0300

    maxtree

--- btreemax.go
+++ btreemax.go
@@ -4,5 +4,11 @@ func BTreeMax(root *TreeNode) *TreeNode {
 	if root == nil {
 		return nil
 	}
+	if root.Right.Data > root.Data {
+		return root.Right
+	}
+	if root.Left.Data > root.Data {
+		return root.Left
+	}
 	return root.Right
 }
