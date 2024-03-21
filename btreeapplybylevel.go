88962f8aea3e694abdc980671e8e4cca376665f3
aosindo <andyovvo8@gmail.com>
2024-03-21 14:43:36 +0300

    map

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -1,17 +1,15 @@
 package piscine
 
-func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error))  {
-
+func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
 	h := BTreeLevelCount(root)
 	for i := 0; i < h; i++ {
 		applyLevel(root, i, f)
 	}
-
 }
 
 func applyLevel(root *TreeNode, level int, f func(...interface{}) (int, error)) {
 	if root == nil {
-		return 
+		return
 	}
 
 	if level == 0 {
