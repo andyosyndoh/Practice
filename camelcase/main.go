94c284afef3a2d9cf2eb1fad29f6877b2a7645f5
aosindo <andyovvo8@gmail.com>
2024-08-01 16:03:02 +0300

    added camel case

--- camelcase/main.go
+++ camelcase/main.go
@@ -13,3 +13,28 @@ func main() {
 	fmt.Println(CamelToSnakeCase("hey2"))
 }
 
+func CamelToSnakeCase(s string) string {
+	str := ""
+	for i, ch := range s {
+		if IsCap(ch) && IsCap(rune(s[i+1])) {
+			return s
+		}
+		if IsCap(ch) && i != 0{
+			str += "_" + string(ch)
+		} else {
+			str += string(ch)
+		}
+
+	}
+	return str
+}
+
+func IsCap(d rune) bool {
+	alph := "QWERTYUIOPLKJHGFDSAZXCVBNM"
+	for _ , let := range alph {
+		if d == let {
+			return true
+		}
+	}
+	return false
+}
