ae04be7cc10e90951641f0ac4ed18a102c7eb111
aosindo <andyovvo8@gmail.com>
2024-10-18 12:17:41 +0300

    chore: added exercises

---
+++ iscapitalized/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(IsCapitalized("Hello and There"))
+	fmt.Println(IsCapitalized("Hello And There"))
+}
+
+func IsCapitalized(str string) bool {
+	if str == "" || Small(rune(str[0])) {
+		return false
+	}
+	for i := 1; i < len(str); i++ {
+		if str[i] == ' ' && Small(rune(str[i+1])) {
+			return false
+		}
+	}
+	return true
+}
+
+func Small(r rune)bool {
+	return r >= 'a' && r <= 'z'
+}
