0d66f07e38848a57cddb5bc2e9dac8a05c4ca295
aosindo <andyovvo8@gmail.com>
2024-11-17 16:05:01 +0300

    added exercises

--- goroutine/main.go
+++ goroutine/main.go
@@ -2,22 +2,22 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 )
 
+var ch =  make(chan string)
+
 func f(n int) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(n, ":", i)
-		amt := time.Duration(rand.Intn(1000))
-		time.Sleep(time.Millisecond * amt)
+	str := ""
+	for i := 0; i < n; i++ {
+		str += fmt.Sprintf("i=%d", i)
 	}
+	ch <- str
 }
 
 func main() {
-	for i := 0; i < 10; i++ {
-		go f(i)
-	}
-	var input string
-	fmt.Scanln(&input)
+	go f(10)
+    go f(5)
+
+    fmt.Println(<-ch) // Output: i=0,i=1,i=2,i=3,i=4,i=5
+    fmt.Println(<-ch) // Output: i=0,i=1,i=2,i=3,i=4,i=5
 }
