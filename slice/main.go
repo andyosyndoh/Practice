af202b5ca13d104d6eb3e47955917859f7fc0e48
aosindo <andyovvo8@gmail.com>
2024-11-22 16:11:13 +0300

    added exercises

--- slice/main.go
+++ slice/main.go
@@ -7,15 +7,6 @@ import (
 	"github.com/01-edu/go-tests/solutions"
 )
 
-// func main() {
-// 	a := []string{"coding", "algorithm", "ascii", "package", "golang"}
-// 	fmt.Printf("%#v\n", Slice(a, 1))
-// 	fmt.Printf("%#v\n", Slice(a, 2, 4))
-// 	fmt.Printf("%#v\n", Slice(a, -3))
-// 	fmt.Printf("%#v\n", Slice(a, -2, -1))
-// 	fmt.Printf("%#v\n", Slice(a, 2, 0))
-// }
-
 func main() {
 	elems := [][]interface{}{
 		{
@@ -72,14 +63,14 @@ func Slice(a []string, nbrs ...int) []string {
 
 	if len(nbrs) >= 2 {
 		end = nbrs[1]
+		if end > len(a) {
+			end = len(a)
+		}
 		if end < 0 {
 			end = len(a) + end
 		}
 		if end < 0 {
 			end = 0
-		}
-		if end > len(a) {
-			end = len(a)
 		}
 		if start > end {
 			return nil
